6db0c63545b962e919d0e60062d76227258410e3
Alecx <99165584+Zumbulw@users.noreply.github.com>
2022-12-16 10:07:22 +0200

    awdawd

--- server.js
+++ server.js
@@ -131,6 +131,21 @@ fastify.get("/logs", async (request, reply) => {
     : reply.view("/src/pages/admin.hbs", params);
 });
 
+fastify.get("/ballers", async (request, reply) => {
+  let params = request.query.raw ? {} : { seo: seo };
+
+  // Get the log history from the db
+  params.optionHistory = await db.getLogs();
+
+  // Let the user know if there's an error
+  params.error = params.optionHistory ? null : data.errorMessage;
+
+  // Send the log list
+  return request.query.raw
+    ? reply.send(params)
+    : reply.view("/src/pages/hey.html", params);
+});
+
 /**
  * Admin endpoint to empty all logs
  *
